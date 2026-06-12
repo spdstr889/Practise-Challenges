5fd9adbd06d6efa8621b939de0ce231255782409
spdstr889 <spdstr889@users.noreply.github.com>
2026-06-12 13:57:12 -0400

    Created new class and arraylist

--- Phase_2/JobAppTracker/JobAppTracker.java
+++ Phase_2/JobAppTracker/JobAppTracker.java
@@ -1,18 +1,25 @@
+package JobAppTracker;
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class JobAppTracker{
+public class JobAppTracker {
+    static ArrayList<Applications> applications=new ArrayList<>();
     public static void main(String[] args){
         Scanner scanner=new Scanner (System.in);
-       
+        int option=0;
+        Applications app =new Applications();
+        while (option !=5){
+
         System.out.println("Menu: ");
         System.out.println("1) Add Application");
         System.out.println("2) Search - by company name or position");
         System.out.println("3) View All Applications");
         System.out.println("4) Update Status");
         System.out.println("5) Exit");
-        int option=scanner.nextInt();
+        option=scanner.nextInt();
 
-        
+       }
+       
     }
 
 }
